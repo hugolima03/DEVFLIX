8a953aecb061dd528759b69ac1e1a016be917c29
HugoLima <hugo8romao@gmail.com>
2020-07-28 16:53:58 -0400

    DIA 2 (Começando o formulário de categoria)

--- devflix/src/pages/cadastro/Categoria/index.js
+++ devflix/src/pages/cadastro/Categoria/index.js
@@ -9,6 +9,19 @@ function CadastroCategoria() {
             <h1>
                 Cadastro de Categoria
             </h1>
+            <form>
+
+                <label>
+                    Nome da Categoria: 
+                    <input
+                    type="text"
+                    />
+                </label>
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>
 
             <Link to="/cadastro/categoria">
                 Ir para home
